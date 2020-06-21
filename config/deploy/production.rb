73c4c646e0883bfed8adf915d4e9c38cd10bf59c
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-06-21 12:38:21 +0900

    deproysettei8

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.148.44', user: 'ec2-user', roles: %w{app db web}
+server "18.178.148.44", user: "ec2-user", roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
