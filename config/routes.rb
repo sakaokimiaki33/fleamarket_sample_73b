64aa7fcd2540c9f1672155f4e45fea51de055b5e
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-06-24 19:27:24 +0900

    Create Devise routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+    # gem 'devise' で使用できるルーティングを定義する。
+
   root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
