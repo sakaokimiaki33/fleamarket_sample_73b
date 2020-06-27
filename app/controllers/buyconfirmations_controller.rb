1e53edbf8c85891a5fe8f09bd5701f6fa26104d3
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-06-27 16:19:31 +0900

    buypage

---
+++ app/controllers/buyconfirmations_controller.rb
@@ -0,0 +1,5 @@
+class BuyconfirmationsController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+
+  resources :buyconfirmations, only: [:index] do
+    collection do
+      get 'index', to: 'buyconfirmations#index'
+    end
+  end
 end
