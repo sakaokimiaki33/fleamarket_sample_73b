8b3961e44a7e2455932160a5624fe9345d81746d
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-07-06 20:47:00 +0900

    dbreset

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_07_04_093043) do
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "toppages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
