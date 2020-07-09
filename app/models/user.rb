d312ce24a4e55dd3801e6ac4e8327709a2161f6e
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-07-10 00:55:02 +0900

    Connect the parent model "User" with the child model "Address".

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  mount_uploader :user_img, UserImageUploader
+  # mount_uploader :user_img, UserImageUploader
   validates :family_name, presence: true
   validates :family_name_kana, presence: true
   validates :name, presence: true
   validates :name_kana, presence: true
   validates :nickname, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 7 }
+  validates :password, presence: true
   validates :birthday, presence: true
 
-  # belongs_to :address, dependent: :destroy
+  has_one :address, inverse_of: :user, dependent: :destroy
+  accepts_nested_attributes_for :address
+    # 親モデルで "accepts_nested_attributes_for :子モデル名" を定義することでフォームから送信した値を親と子のテーブルに同時に保存する。
   # has_many :cards, dependent: :destroy
   # has_many :items, dependent: :destroy
   # has_many :comments, dependent: :destroy
