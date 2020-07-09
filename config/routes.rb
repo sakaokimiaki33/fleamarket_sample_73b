199c15b58de5cfb791cac63c8e4c7a4e97443a9d
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-07-09 20:41:59 +0900

    cherry-pick3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   devise_for :addresses
+  get 'card/new'
+  get 'card/show'
   devise_for :users
   root to: 'items#index'
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   
 
   resources :buyconfirmations, only: [:index] do
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2020_07_09_110518) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "ancestry"
