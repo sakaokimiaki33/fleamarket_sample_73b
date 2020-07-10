4dd238155cc30137014234f02d30c60c7032a8d9
zuker-noi <inoshishi44@gmail.com>
2020-07-10 22:35:18 +0900

    end of coding outline

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.save!
       redirect_to root_path
     else
       redirect_to new_item_path
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  has_many :comments, dependent: :destroy
-  belongs_to :user
-  belongs_to :category
+  # has_many :comments, dependent: :destroy
+  # belongs_to :user
+  # belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
