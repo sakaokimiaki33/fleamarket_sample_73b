4032b9558cd269fca8a480f0587960dc3c27f2c6
zuker-noi <inoshishi44@gmail.com>
2020-07-29 15:59:05 +0900

    finish uploading images

--- app/assets/javascripts/item-new.js
+++ app/assets/javascripts/item-new.js
@@ -1,16 +1,13 @@
 $(document).on('turbolinks:load', function(){
   $(function(){
 
-    //プレビューのhtmlを定義
+    //プレビューのhtml
     function buildHTML(count) {
       var html = `<div class="preview-box" id="preview-box__${count}">
                     <div class="upper-box">
                       <img src="" alt="preview">
                     </div>
                     <div class="lower-box">
-                      <div class="update-box">
-                        <label class="edit_btn">編集</label>
-                      </div>
                       <div class="delete-box" id="delete_btn_${count}">
                         <span>削除</span>
                       </div>
