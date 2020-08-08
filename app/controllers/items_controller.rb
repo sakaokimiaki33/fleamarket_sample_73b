586e7e3185e2a29fa75464ca88376ce44f974f49
zuker-noi <inoshishi44@gmail.com>
2020-08-08 17:48:25 +0900

    fix render page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,8 +30,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_update_params)
-    redirect_to root_path
+    if @item.update(item_update_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
