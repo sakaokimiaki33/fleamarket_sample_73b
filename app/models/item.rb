cee9ae3dedebee8f61b3d447ee98a47e5e513c32
zuker-noi <inoshishi44@gmail.com>
2020-08-16 15:12:06 +0900

    created delete function

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   # has_many :comments, dependent: :destroy
   # belongs_to :category
   # belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   
   extend ActiveHash::Associations::ActiveRecordExtensions
