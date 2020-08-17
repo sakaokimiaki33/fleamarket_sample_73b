b1e7b53dfed143c6b743c3d56a6fa382025ac6e6
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-08-17 21:10:38 +0900

    fix4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_022551) do
+ActiveRecord::Schema.define(version: 2020_07_09_110518) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name_deliver", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_022551) do
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,10 +65,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_022551) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    # t.index ["category_id"], name: "index_items_on_category_id"
-    t.string "image"
-    t.integer "saler"
-    t.integer "buyer"
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
